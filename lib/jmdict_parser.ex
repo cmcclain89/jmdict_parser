db01f26e7d5667b8b524af397857bb9b8443ee28
Caleb <caleb.mcclain89@gmail.com>
2024-07-07 21:20:26 +0900

    adding

---
+++ lib/jmdict_parser.ex
@@ -0,0 +1,18 @@
+defmodule JmdictParser do
+  @moduledoc """
+  Documentation for `JmdictParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JmdictParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jmdict_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule JmdictParserTest do
+  use ExUnit.Case
+  doctest JmdictParser
+
+  test "greets the world" do
+    assert JmdictParser.hello() == :world
+  end
+end
